453628ecb892a1b84b56eb2a605c921c3edecb05
Rian Ihsan Ardiansyah <93025581+RianIhsan@users.noreply.github.com>
2023-10-28 15:10:42 +0700

    fix: load env

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	// "log"
-
+	"log"
+	"os"
 	"github.com/RianIhsan/raise-unity/routes"
 	"github.com/RianIhsan/raise-unity/utils/database"
 	"github.com/RianIhsan/raise-unity/utils/migration"
 	"github.com/gin-gonic/gin"
-	// "github.com/joho/godotenv"
+	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -15,6 +15,14 @@ func main() {
 	// if err != nil {
 	// 	log.Fatal("Failed to fetch .env file")
 	// }
+
+	_, err := os.Stat(".env")
+	if err == nil {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Failed to fetch .env file")
+		}
+	}
 	database.InitDB()
 	migration.GoMigrate()
 
